e3a9b59da492bb9bbc58b42a72bd8069fa7cab84
Alfred Hult <alfhu925@student.liu.se>
2018-04-24 08:10:48 +0200

    All cleaned up

--- styrmodul_halv_duplex/styrmodul_halv_duplex/Debug/makedep.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit or delete the file
-################################################################################
-
-alfred_monkaS.c
-
-styrmodul_halv_duplex.c
-
